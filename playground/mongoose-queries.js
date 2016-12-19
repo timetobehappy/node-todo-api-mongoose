5edaf9a4adad6a59a1d2a2eaf74408b84e9d444e
timetobehappy <jibiu@outlook.com>
2016-12-19 11:34:46 -0600

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+const {ObjectID} = require('mongodb'); // This is just required when you need to validate the ObjectID
+
+
+const {
+    mongoose
+} = require('./../server/db/mongoose');
+const {
+    Todo
+} = require('./../server/models/todo');
+const {
+    User
+} = require('./../server/models/user');
+
+var id = '58574922e447e3191c09b085';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID is not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos)=>{
+//   console.log('Todos',todos);
+// });
+//
+//
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo)=>{
+//   console.log('Todo',todo);
+// });
+
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+
+    console.log('Todo by id', todo);
+}).catch((e) => console.log(e));
