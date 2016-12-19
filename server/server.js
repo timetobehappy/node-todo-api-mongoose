16711c3309526a87a66f3456709d8a3b260cef6d
timetobehappy <jibiu@outlook.com>
2016-12-19 11:50:39 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+    ObjectID
+} = require('mongodb'); // This is just required when you need to validate the ObjectID
+
 
 var {
     mongoose
@@ -17,29 +21,56 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  //console.log(req.body);
-  var todo = new Todo({
-    text: req.body.text
-  });
+    //console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-  todo.save().then((doc)=>{
-    res.send(doc);
-  },(err)=>{
-    res.status(400).send(err);
-  })
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
 
 });
 
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos)=>{
-    res.send({
-      todos
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (err) => {
+        res.status(400).send(err);
     });
-  },(err)=>{
-    res.status(400).send(err);
-  });
 });
+
+
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            todo
+        });
+
+    }).catch((e) => {
+        res.status(400).send()
+    });
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Started app on port 3000');
